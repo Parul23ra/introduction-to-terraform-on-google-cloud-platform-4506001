28deaa8f46dba82b373f90770a4e970ffa2f4a1f
Parul23ra <paarul.sharma28@gmail.com>
2024-08-05 07:12:32 -0400

    update variable

--- variables.tf
+++ variables.tf
@@ -1,41 +1,41 @@
 
-Variables "project_id" {
+Variable "project_id" {
   type     = string
   default  = "terraform-gcp-431111"
 }
 
-Variables "region" {
+Variable "region" {
   type     = string
   default  = "us-west1"
 }
 
-Variables "network_name" {
+Variable "network_name" {
   type     = string
   default  = "app"
 }
 
-Variables "network_ip_range" {
+Variable "network_ip_range" {
   type     = string
   default  = "10.2.0.0/16"
 }
 
-Variables "image_project" {
+Variable "image_project" {
   type     = string
   default  = "ubuntu-os-cloud"
 }
 
-Variables "image_family" {
+Variable "image_family" {
   type     = string
   default  = "ubuntu-2204-lts"
 }
 
 
-Variables "app_name" {
+Variable "app_name" {
   type     = string
   default  = "blog"
 }
 
-Variables "machine_type" {
+Variable "machine_type" {
   type     = string
   default  = "e2-micro"
 }
